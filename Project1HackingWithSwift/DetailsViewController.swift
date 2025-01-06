1c5d4fc4c4a69c64d7488f5f2d19cb8a1462e61a
Basant <basantsalama05@gmail.com>
2025-01-06 18:41:50 +0200

    Added sharing functionality using UIActivityViewController - Implemented shareTapped() method in DetailViewController - Added UIBarButtonItem to navigation bar for sharing - Updated Info.plist to include permission for saving images

--- Project1HackingWithSwift/DetailsViewController.swift
+++ Project1HackingWithSwift/DetailsViewController.swift
@@ -17,9 +17,29 @@ class DetailsViewController: UIViewController {
         if let imageToLOad = selectedImage{
             
             imageView.image = UIImage(named: imageToLOad)
+
         }
+        
+        
+        
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
     }
     
+    
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+
+
 
 
 }
